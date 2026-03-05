b8042fbf8a5fde0d7d098c3d18361d79da5f701b
Jason-CloudG <135985363+Jason-CloudG@users.noreply.github.com>
2026-03-05 11:45:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,13 @@
+variable "org_id" {}
+variable "billing_account" {}
+variable "customer_name" {}
 
+variable "environments" {
+  type = list(string)
+}
+
+variable "network_model" {}
+variable "iam_model" {}
+variable "budget_threshold" {}
+
+variable "bootstrap_project" {}

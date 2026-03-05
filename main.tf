896f92be4eabf1a0ab42d65cc928c7b7eb557661
Jason-CloudG <135985363+Jason-CloudG@users.noreply.github.com>
2026-03-05 11:46:22 +0530

    Add multiple modules for organization and resources

--- main.tf
+++ main.tf
@@ -1,1 +1,40 @@
+module "org_structure" {
+  source = "./modules/org_structure"
 
+  org_id        = var.org_id
+  customer_name = var.customer_name
+}
+
+module "projects" {
+  source = "./modules/projects"
+
+  billing_account = var.billing_account
+  environments    = var.environments
+  customer_name   = var.customer_name
+}
+
+module "networking" {
+  source = "./modules/networking"
+
+  network_model = var.network_model
+  environments  = var.environments
+}
+
+module "iam" {
+  source = "./modules/iam"
+
+  iam_model = var.iam_model
+}
+
+module "logging" {
+  source = "./modules/logging"
+
+  org_id = var.org_id
+}
+
+module "budgets" {
+  source = "./modules/budgets"
+
+  billing_account = var.billing_account
+  threshold       = var.budget_threshold
+}
